59444e0f6f9ba2dec87dc7c954d7f579d4cec6e8
UnderKo <UnderKo@DESKTOP-ATCVI05>
2024-04-13 07:49:35 +0300

    das

--- SoundRecording/MainWindow.xaml.cs
+++ SoundRecording/MainWindow.xaml.cs
@@ -10,6 +10,29 @@ namespace SoundRecording
         {
             InitializeComponent();
         }
+        private void CloseClick(object sender, RoutedEventArgs e)
+        {
+            Window window = (Window)((FrameworkElement)sender).TemplatedParent;
+            window.Close();
+        }
+        private void MaximizeRestoreClick(object sender, RoutedEventArgs e)
+        {
+            Window window = (Window)((FrameworkElement)sender).TemplatedParent;
+            if (window.WindowState == System.Windows.WindowState.Normal)
+            {
+                window.WindowState = System.Windows.WindowState.Maximized;
+            }
+            else
+            {
+                window.WindowState = System.Windows.WindowState.Normal;
+            }
+        }
+
+        private void MinimizeClick(object sender, RoutedEventArgs e)
+        {
+            Window window = (Window)((FrameworkElement)sender).TemplatedParent;
+            window.WindowState = System.Windows.WindowState.Minimized;
+        }
         protected override void OnSourceInitialized(EventArgs e)
         {
             base.OnSourceInitialized(e);
